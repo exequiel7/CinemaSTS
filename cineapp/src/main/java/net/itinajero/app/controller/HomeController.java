d9aa76373ff9255b253191ec597878d909b164c2
Exequiel Deferrari <exedeferrari@gmail.com>
2019-07-12 15:42:53 -0300

    2019-07-12 15:42 - Commit: diseño tabla con Bootstrap

--- cineapp/src/main/java/net/itinajero/app/controller/HomeController.java
+++ cineapp/src/main/java/net/itinajero/app/controller/HomeController.java
@@ -1,5 +1,6 @@
 package net.itinajero.app.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import net.itinajero.app.model.Pelicula;
 
 @Controller
 public class HomeController {
@@ -18,12 +21,13 @@ public class HomeController {
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String mostrarPrincipal(Model model) {
-		List<String> peliculas = new LinkedList<>(); // lista enlazada
+		List<Pelicula> peliculas = getLista();
 		
-		peliculas.add("Rapido y Furioso");
-		peliculas.add("El aro 2");
-		peliculas.add("Aliens");
+//		peliculas.add("Rapido y Furioso");
+//		peliculas.add("El aro 2");
+//		peliculas.add("Aliens");
 		model.addAttribute("peliculas", peliculas);
+		
 		return "home";
 	}
 	
@@ -39,6 +43,49 @@ public class HomeController {
 		
 		return "detalle";
 		
+	}
+	
+	// Metodo para generar una lista de objetos de Modelo (Pelicula)
+	private List<Pelicula> getLista(){
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+		List<Pelicula> listaPeliculas = null;
+		
+		try {
+			listaPeliculas = new LinkedList<>();
+			
+			Pelicula peli1 = new Pelicula();
+			peli1.setId(1);
+			peli1.setTitulo("Power Rangers");
+			peli1.setDuracion(120);
+			peli1.setClasificacion("B");
+			peli1.setGenero("Aventura");
+			peli1.setFechaEstreno(formatter.parse("15-07-2019"));
+			
+			Pelicula peli2 = new Pelicula();
+			peli2.setId(1);
+			peli2.setTitulo("La Bella y la Bestia");
+			peli2.setDuracion(132);
+			peli2.setClasificacion("A");
+			peli2.setGenero("Infantil");
+			peli2.setFechaEstreno(formatter.parse("20-07-2019"));
+			
+			Pelicula peli3 = new Pelicula();
+			peli3.setId(1);
+			peli3.setTitulo("Contratiempo");
+			peli3.setDuracion(106);
+			peli3.setClasificacion("B");
+			peli3.setGenero("Thriller");
+			peli3.setFechaEstreno(formatter.parse("25-07-2019"));
+			
+			listaPeliculas.add(peli1);
+			listaPeliculas.add(peli2);
+			listaPeliculas.add(peli3);
+			
+			return listaPeliculas;
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+			return null;
+		}
 	}
 	
 }
--- cineapp/webapp/WEB-INF/views/home.jsp
+++ cineapp/webapp/WEB-INF/views/home.jsp
@@ -1,13 +1,19 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Bienvenido a CineApp</title>
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+	crossorigin="anonymous">
+
 </head>
 <body>
+	<%--
 	<h1>Lista de Peliculas</h1>
 	
 	<ol>
@@ -15,5 +21,41 @@
 			<li>${pelicula}</li>
 		</c:forEach>
 	</ol>
+	 --%>
+
+	<div class="card">
+		<div class="card-header alert alert-primary" >Tabla de peliculas</div>
+		<div class="card-body">
+			<table class="table table-striped table-bordered table-hover">
+				<thead>
+					<!--header in html table-->
+					<tr>
+						<th>Id</th>
+						<th>Título</th>
+						<th>Duración</th>
+						<th>Clasificación</th>
+						<th>Género</th>
+						<th>Imagen</th>
+						<th>Fecha de Estreno</th>
+						<th>Estado</th>
+					</tr>
+				</thead>
+				<tbody>
+					<c:forEach items="${peliculas}" var="pelicula">
+						<tr>
+							<td>${pelicula.id}</td>
+							<td>${pelicula.titulo}</td>
+							<td>${pelicula.duracion}min.</td>
+							<td>${pelicula.clasificacion}</td>
+							<td>${pelicula.genero}</td>
+							<td>${pelicula.imagen}</td>
+							<td>${pelicula.fechaEstreno}</td>
+							<td>${pelicula.estado}</td>
+						</tr>
+					</c:forEach>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </body>
 </html>
